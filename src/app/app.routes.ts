d67c487dd3e2189bdacd6786e5741fd1ccbcb41c
PijusVose <pijus.vose@gmail.com>
2024-03-24 21:53:19 +0200

    finished Task 7

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { AccountComponent } from './account/account.component';
 import { filterAccessToAccountGuard } from './guards/filter-access-to-account.guard';
 import { AccountListComponent } from './account-list/account-list.component';
 import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 export const routes: Routes = [
   { path: 'account-name/:name', component: AccountListComponent },
@@ -13,6 +15,9 @@ export const routes: Routes = [
   { path: 'accounts/:id', component: AccountComponent, canActivate: [filterAccessToAccountGuard] },
   { path: 'faq', component: FaqComponent },
   { path: 'contacts', component: ContactsComponent },
-  { path: 'auth', component: AuthComponent },
+  { path: 'auth', component: AuthComponent, children: [
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent }
+  ] },
   { path: "**", component: NotfoundComponent }
 ];
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
 export class AuthComponent {
+  constructor(private router: Router) {}
 
+  onRouteBtnClicked(routePath: string) {
+    this.router.navigate(['auth', routePath]);
+  }
 }
